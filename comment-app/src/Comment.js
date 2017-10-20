bf55ad6f16b328ce6f5bff9c9ceba6dbed037978
yinzhuoya <yinzhuoya@foxmail.com>
2017-10-20 21:35:59 +0800

    comment-app 6

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 
 class Comment extends Component {
   static PropTypes = {
-    comment: PropTypes.object.isRequired
+    comment: PropTypes.object.isRequired,
+    onDeleteComment: PropTypes.func,
+    index: PropTypes.number,
   }
 
   constructor () {
@@ -19,6 +21,10 @@ class Comment extends Component {
     )
   }
 
+  componentWillUnmount () {
+    clearInterval(this._timer)
+  }
+
   _updateTimeString () {
     const comment = this.props.comment
     const duration = (+Date.now() - comment.createdTime) / 1000
@@ -27,15 +33,37 @@ class Comment extends Component {
     })
   }
 
+  handleDeleteComment () {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(this.props.index)
+    }
+  }
+
+  _getProcessedContent (content) {
+    return content
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;")
+    .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+  }
+
   render () {
+    const { comment } = this.props
     return (
       <div className='comment'>
         <div className='comment-user'>
           <span>{this.props.comment.username}：</span>
         </div>
-        <p>{this.props.comment.content}</p>
+        <p dangerouslySetInnerHTML={{ __html: this._getProcessedContent(comment.content)}}></p>
         <span className='comment-createdtime'>
           {this.state.timeString}
+        </span>
+        <span 
+          className='comment-delete'
+          onClick={this.handleDeleteComment.bind(this)}>
+          删除
         </span>
       </div>
     )
--- comment-app/src/CommentApp.js
+++ comment-app/src/CommentApp.js
@@ -38,12 +38,22 @@ class CommentApp extends Component {
     console.log(comment)
   }
 
+  handleDeleteComment (index) {
+    console.log(index)
+    const comments = this.state.comments
+    comments.splice(index, 1)
+    this.setState({ comments })
+    this._saveComments(comments)
+  }
+
   render() {
     return (
       <div className="wrapper">
         <CommentInput
           onSubmit={this.handleSubmitComment.bind(this)} />
-        <CommentList  comments={this.state.comments}/>
+        <CommentList  
+          comments={this.state.comments}
+          onDeleteComment={this.handleDeleteComment.bind(this)}/>
       </div>
     )
   }
