388150ce09f14b496c4c9eb018027bf8f49c8cca
yinzhuoya <yinzhuoya@foxmail.com>
2017-10-24 23:57:15 +0800

    Redux (四)

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,5 +1,6 @@
 import './index.css'
 
+let log = console.log.bind(console)
 
 let appState = {
   title: {
@@ -17,7 +18,7 @@ function createStore(state, stateChanger) {
   const subscribe = (listener) => listeners.push(listener)
   const getState = () => state
   const dispatch = (action) => {
-    stateChanger(state, action)
+    state = stateChanger(state, action)
     listeners.forEach((listener) => listener())
   }
   return { getState, dispatch, subscribe}
@@ -26,35 +27,56 @@ function createStore(state, stateChanger) {
 function stateChanger(state, action) {
   switch(action.type) {
     case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state
   }
 }
 
-function renderApp(appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
+function renderApp(newAppState, oldAppState = {}) {
+  if(newAppState === oldAppState) return
+  log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle(title) {
+function renderTitle(newTitle, oldTitle = {}) {
+  if(newTitle === oldTitle) return
+  log('render title...')
   const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
 }
 
-function renderContent(content) {
+function renderContent(newContent, oldContent = {}) {
+  if(newContent === oldContent) return
+  log('render content...')
   const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
 }
 
 const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState()))
+let oldState = store.getState()
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+})
 
 renderApp(store.getState())
 store.dispatch({type:'UPDATE_TITLE_TEXT',text:'《React.js 小书》'})
