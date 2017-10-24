6422bd53dfb832fd27164b8cd1b668e5ea9113ca
yinzhuoya <yinzhuoya@foxmail.com>
2017-10-25 00:39:13 +0800

    make-react-redux 1

---
+++ make-react-redux/src/Header.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+class Header extends Component {
+  render () {
+    return(
+      <h1>React.js小书</h1>
+    )
+  }
+}
+
+export default Header
--- make-react-redux/src/index.js
+++ make-react-redux/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Content from './Content'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Index/>,
+  document.getElementById('root')
+)
