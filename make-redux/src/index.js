00de093f2bf0059e3df45c99c54d043265b2c240
yinzhuoya <yinzhuoya@foxmail.com>
2017-10-24 17:31:50 +0800

    Redux (一)

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+let appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue',
+  }
+}
+
+function dispatch(action) {
+  switch(action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+function renderApp(appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle(title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+
+renderApp(appState)
+
+setTimeout(function(){
+  dispatch({type: 'UPDATE_TITLE_TEXT',text:'《React.js 小书》'})
+  dispatch({type: 'UPDATE_TITLE_COLOR',color:'blue'})
+  renderApp(appState)
+},1000)
