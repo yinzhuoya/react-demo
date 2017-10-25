48bc20f411b18bc6060118a689d0ae4609f34450
yinzhuoya <yinzhuoya@foxmail.com>
2017-10-25 14:23:16 +0800

    make-react-redux 2

--- make-react-redux/src/Header.js
+++ make-react-redux/src/Header.js
@@ -1,9 +1,30 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Header extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor () {
+    super()
+    this.state = { themeColor: ''}
+  }
+
+  componentWillMount () {
+    const {store} = this.context
+    this._updateThemeColor()
+    store.subscribe(() => this._updateThemeColor())
+  }
+
+  _updateThemeColor () {
+    const { store } = this.context
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor})
+  }
   render () {
     return(
-      <h1>React.js小书</h1>
+      <h1 style={{ color: this.state.themeColor }}>React.js小书</h1>
     )
   }
 }
